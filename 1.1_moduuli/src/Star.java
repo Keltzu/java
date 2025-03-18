9b089cda02f8a334276ed09b08205660531ab7ae
Keltzu <kalleto@metropolia.fi>
2025-03-18 11:07:01 +0200

    Task_2

---
+++ 1.1_moduuli/src/Star.java
@@ -0,0 +1,10 @@
+// Task 2
+public class Star {
+    public static void main(String[] args) {
+        // Printtaa tähdet :3
+        System.out.println("    *");
+        System.out.println("   ***");
+        System.out.println("  *****");
+        System.out.println(" *******");
+    }
+}
